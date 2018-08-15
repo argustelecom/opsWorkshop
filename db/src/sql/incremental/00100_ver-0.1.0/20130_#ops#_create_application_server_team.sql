82071a8b2f3e3be679af84dc46ea1a57a9585e80
Константин Короповский <k.koropovskiy@argustelecom.ru>
2018-08-15 16:03:37 +0300

    ошибки при создании БД

--- db/src/sql/incremental/00100_ver-0.1.0/20130_#ops#_create_application_server_team.sql
+++ db/src/sql/incremental/00100_ver-0.1.0/20130_#ops#_create_application_server_team.sql
@@ -1,7 +1,7 @@
 create table ops.application_server_team (
 		appserver_id BIGINT NOT NULL,
 		team_id BIGINT NOT NULL,
-	CONSTRAINT pk_application_server_team PRIMARY KEY (application_server_id, team_id),
+	CONSTRAINT pk_application_server_team PRIMARY KEY (appserver_id, team_id),
 	CONSTRAINT fk_team FOREIGN KEY (team_id) REFERENCES ops.team (id),
 	CONSTRAINT fk_application_sever FOREIGN KEY (appserver_id) REFERENCES ops.application_server (id)
     );
